2f07d43043d1e16b404544ef675faa950911384e
Wulan Nur Annisah <wulannurannisah470@gmail.com>
2021-11-18 07:23:22 +0700

    Add files via upload

---
+++ P2_1_1204007/P2_1_1204007/Dvd_1204007.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204007
+{
+    public class Dvd_1204007 : Product_1204007
+    {
+        protected string duration;
+
+        public Dvd_1204007(string title, string duration)
+        {
+            this.MyTitle = title;
+            this.Duration = duration;
+            this.MyType = "DVD";
+
+            Console.WriteLine("Ini dari class DVD");
+        }
+
+        public string Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+            }
+        }
+    }
+}
