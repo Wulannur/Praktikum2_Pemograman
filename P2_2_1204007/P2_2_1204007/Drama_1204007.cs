4565e3e53c5a0ee9c8769b93de9577e0325f2174
Wulan Nur Annisah <wulannurannisah470@gmail.com>
2021-11-18 07:25:46 +0700

    Add files via upload

---
+++ P2_2_1204007/P2_2_1204007/Drama_1204007.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204007
+{
+    public class Drama_1204007 : Product_1204007
+    {
+        protected string duration;
+
+        public Drama_1204007(string title, string duration)
+        {
+            this.MyTitle = title;
+            this.Duration = duration;
+            this.MyType = "Drama";
+
+            Console.WriteLine("Drama Rekomendasi");
+        }
+
+        public string Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+            }
+        }
+    }
+}
