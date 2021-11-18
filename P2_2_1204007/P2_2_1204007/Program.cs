0ee4bc9b22072d0860b302b8586ebf4ae8976036
Wulan Nur Annisah <wulannurannisah470@gmail.com>
2021-11-18 07:26:46 +0700

    Add files via upload

---
+++ P2_2_1204007/P2_2_1204007/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204007
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Wattpad_1204007 mywattpad = new Wattpad_1204007("Wattpad", "Calon Imam", "186");
+            Drama_1204007 drama = new Drama_1204007("EXIT", "104");
+
+            Console.WriteLine("Product 1 is a {0} called \"{1}\" and has {2} pages", mywattpad.MyType, mywattpad.MyTitle, mywattpad.PageCount);
+            Console.WriteLine("Product 1 is a {0} called \"{1}\" and has {2} minute duration", drama.MyType, drama.MyTitle, drama.Duration);
+        }
+    }
+}
---
+++ P2_2_1204007/P2_2_1204007/Wattpad_1204007.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204007
+{
+    public class Wattpad_1204007 : Product_1204007
+    {
+        protected string pageCount;
+
+        public Wattpad_1204007(string mytype, string mytitle, string pagecount) : base(mytype, mytitle)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+
+            {
+                return pageCount;
+            }
+
+            set
+            {
+                pageCount = value;
+            }
+        }
+
+    }
+}
