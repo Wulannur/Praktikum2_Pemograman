e2016b3f16049dbbdfb45b9691386a9230cd396d
Wulan Nur Annisah <wulannurannisah470@gmail.com>
2021-11-18 07:28:02 +0700

    Add files via upload

---
+++ P2_1_1204007/P2_1_1204007/Product_1204007.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204007
+{
+    public class Product_1204007
+    {
+        private string myType;
+        private string myTitle;
+
+        public Product_1204007()
+        {
+
+        }
+
+        public Product_1204007(string mytype, string mytitle)
+        {
+            this.myType = mytype;
+            this.myTitle = mytitle;
+        }
+
+
+        public string MyType
+        {
+            get
+            {
+                return myType;
+            }
+
+            set
+            {
+                myType = value;
+            }
+        }
+
+        public string MyTitle
+        {
+            get
+            {
+                return myTitle;
+            }
+
+            set
+            {
+                myTitle = value;
+            }
+        }
+    }
+}
